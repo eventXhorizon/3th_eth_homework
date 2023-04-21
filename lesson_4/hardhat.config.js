f31963d345370aa07fa1c40108c9a9aeeb3fa41a
eventhorizon <beingcrazy1874@gmail.com>
2023-04-21 20:32:10 +0800

    init

--- lesson_4/hardhat.config.js
+++ lesson_4/hardhat.config.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const GOERLI_URL = process.env.GOERLI_URL;
+const ETHERSCAN_API = process.env.ETHERSCAN_API;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -17,5 +18,8 @@ module.exports = {
       url: GOERLI_URL,
       accounts: [PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API
   }
 };
